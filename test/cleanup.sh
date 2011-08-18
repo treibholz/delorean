4f404ed4e008925e6c5c7a26142aba2fffd0a133
Klaus Umbach <treibholz-github@uxix.de>
2011-08-18 09:38:00 +0200

    good morning!

--- test/cleanup.sh
+++ test/cleanup.sh
@@ -15,22 +15,55 @@ case "$1" in
 	;;
 esac
 
-cd /tmp/delorean/
+cd /tmp/delorean/ || exit 1
+
+weekago=$(date -d '1 week ago' +%s)
+monthago=$(date -d '1 month ago' +%s)
+yearago=$(date -d '1 year ago' +%s)
+now=$(date +%s)
+
+# Timeperiods in seconds
+DAY=$((60*60*24))
+WEEK=$((DAY*7))
+MONTH=$((DAY*30))
+YEAR=$((DAY*365))
+
 
 find -maxdepth 4 | while read i; do
 
-	if   DATE=$(date -d "${i:2:10} ${i:13:2}:${i:16:2}" +%s 2> /dev/null); then
-		echo $DATE
+#	TIMEFORMAT="+%Y%m%W%d%H%M"
+	TIMEFORMAT="+%s"
+	# DATE is the date encoded in the path of the backups converted to YYYYMMWWDDHHmm
+	if   DATE=$(date -d "${i:2:10} ${i:13:2}:${i:16:2}" $TIMEFORMAT 2> /dev/null); then
+		true
+	elif DATE=$(date -d "${i:2:10}" $TIMEFORMAT 2>/dev/null); then
+		#echo $DATE > /dev/null
+		true
 
-	elif DATE=$(date -d "${i:2:10}" +%s 2>/dev/null); then
-		echo $DATE
-	elif DATE=$(date -d "${i:2:7}" +%s 2> /dev/null); then
-		echo $DATE
+	elif DATE=$(date -d "${i:2:7}" $TIMEFORMAT 2> /dev/null); then
+		#echo $DATE
+		true
+
 	else
 		echo $i
 	fi
-	
 
+	timediff=$((now - DATE))
+
+	years_ago=$((timediff / YEAR))
+	months_ago=$((timediff / MONTH))
+	weeks_ago=$((timediff / WEEK))
+	days_ago=$((timediff / DAY))
+
+	echo $days_ago
+
+	# delete if older than one year. Who really needs Backups that age?
+	#if [ $DATE -lt $yearago ]; then
+	#	echo $i
+	# delete if older than one month if one os already spared
+	#elif [ $DATE -lt $monthago ]; then
+	#	true
+	#fi 
 
 done
 
